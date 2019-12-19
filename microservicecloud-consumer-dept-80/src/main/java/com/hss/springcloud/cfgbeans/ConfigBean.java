b8d5cce604b0dd3cb16046f0358f02de5c76f3ca
HeTreeTerr <3110708879@qq.com>
2019-12-19 23:28:33 +0800

    负载均衡策略

--- microservicecloud-consumer-dept-80/src/main/java/com/hss/springcloud/cfgbeans/ConfigBean.java
+++ microservicecloud-consumer-dept-80/src/main/java/com/hss/springcloud/cfgbeans/ConfigBean.java
@@ -1,5 +1,7 @@
 package com.hss.springcloud.cfgbeans;
 
+import com.netflix.loadbalancer.IRule;
+import com.netflix.loadbalancer.RandomRule;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,5 +14,11 @@ public class ConfigBean {
     @LoadBalanced   //Spring Cloud Ribbon是基于Netflix Ribbon实现的一套客户端  负载均衡的工具。
     public RestTemplate getRestTemplate(){
         return new RestTemplate();
+    }
+
+    @Bean
+    public IRule myRule(){
+        //自定义负载均衡规则
+        return new RandomRule();
     }
 }
