408c45d0f199ede55ab29457c5e9d2f8ffcf91bc
hesensen <hesensen@hztianque.com>
2019-12-28 21:02:41 +0800

    自定义负载均衡

---
+++ microservicecloud-consumer-dept-80/src/main/java/com/hss/myrule/MySelfRule.java
@@ -0,0 +1,25 @@
+package com.hss.myrule;
+
+import com.netflix.loadbalancer.IRule;
+import com.netflix.loadbalancer.RandomRule;
+import com.netflix.loadbalancer.RoundRobinRule;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * 用户自己书写的Ribbon负载均衡配置规则不能和
+ * springCloud的主启动类（@ComponentScan所扫描）的当前包下以及子包下。
+ * 否则我们的自定义的这个配置类会被所有的Ribbon客户端所共享。也就是
+ * 说我们达不到特殊化定制的目的了。
+ */
+@Configuration
+public class MySelfRule {
+
+    @Bean
+    public IRule myRule(){
+        //随机
+        //return new RandomRule();
+        //默认轮询
+        return new RoundRobinRule();
+    }
+}
--- microservicecloud-consumer-dept-80/src/main/java/com/hss/springcloud/DeptConsumer80App.java
+++ microservicecloud-consumer-dept-80/src/main/java/com/hss/springcloud/DeptConsumer80App.java
@@ -1,15 +1,25 @@
 package com.hss.springcloud;
 
+import com.hss.myrule.MySelfRule;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
+import org.springframework.cloud.netflix.ribbon.RibbonClients;
 
 /**
  * 消费者
  * 开启识别消费者@EnableDiscoveryClient
+ * @RibbonClients标签用法：
+ * @RibbonClients(value = {
+ *         @RibbonClient(name = "ribbon-client",configuration = MyConfiguration.class),
+ *         @RibbonClient(name = "ribbon-client-b",configuration = MyConfiguration.class)
+ * })
  */
 @EnableDiscoveryClient
 @SpringBootApplication
+//在启动该微服务的时候就能去加载我们自定义的Ribbon配置类，从而使配置生效
+@RibbonClient(name = "MICROSERVICECLOUD-DEPT",configuration =MySelfRule.class )
 public class DeptConsumer80App {
 
     public static void main(String[] args) {
