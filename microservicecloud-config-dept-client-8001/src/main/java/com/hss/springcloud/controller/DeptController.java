d07fbc5e8ecfc64b3e05fb22ee67f32212440a16
hesensen <hesensen@hztianque.com>
2020-01-13 11:20:43 +0800

    springCloud提供者获取config远程动态配置文件

---
+++ microservicecloud-config-dept-client-8001/src/main/java/com/hss/springcloud/controller/DeptController.java
@@ -0,0 +1,67 @@
+package com.hss.springcloud.controller;
+
+import com.hss.springcloud.entities.Dept;
+import com.hss.springcloud.service.DeptService;
+
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class DeptController {
+
+    private final static Logger logger = Logger.getLogger(DeptController.class);
+
+    @Autowired
+    private DeptService deptService;
+
+    @Autowired
+    private DiscoveryClient discoveryClient;
+
+    @RequestMapping(value = "/dept/add",method = RequestMethod.POST)
+    public Boolean add(@RequestBody Dept dept){
+        logger.info("执行添加部门信息");
+        return deptService.addDept(dept);
+    }
+
+    @RequestMapping(value = "/dept/get/{id}",method = RequestMethod.GET)
+    public Dept get(@PathVariable("id") Long id){
+        logger.info("由id查找部门信息");
+        return deptService.findById(id);
+    }
+
+    @RequestMapping(value = "/dept/list",method = RequestMethod.GET)
+    public List<Dept> list(){
+        logger.info("查找所有部门信息");
+        return deptService.findAll();
+    }
+
+    @RequestMapping(value = "/dept/delete/{id}",method = RequestMethod.DELETE)
+    public Boolean delete(@PathVariable("id") Long id){
+        logger.info("查找所有部门信息");
+        return deptService.deleteDept(id);
+    }
+
+    @RequestMapping(value = "/dept/addDeptKey",method = {RequestMethod.POST})
+    public Dept addDeptKey(@RequestBody Dept dept){
+        logger.info("执行添加部门信息");
+        return deptService.addDeptKey(dept);
+    }
+
+    @RequestMapping(value = "/dept/discovery",method = RequestMethod.GET)
+    public Object discovery(){
+        List<String> list = discoveryClient.getServices();
+        logger.info("********"+list);
+
+        List<ServiceInstance> srvList = discoveryClient.getInstances("MICROSERVICECLOUD-DEPT");
+        for(ServiceInstance element : srvList){
+            logger.info(element.getServiceId()+ "\t" +element.getHost()+ "\t"
+                + element.getPort() +"\t"+element.getUri());
+        }
+        return this.discoveryClient;
+    }
+}
